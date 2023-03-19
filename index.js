b2d1e0010a35d32a868db797abfd984ed63b8ab0
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-03-19 19:41:41 +0600

    created some api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./Data/Categories.json');
+const news = require('./Data/News.json');
 
 app.use(cors());
 
@@ -13,8 +14,28 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+});
+
+app.get('/news', (req, res) => {
+    res.send(news);
 })
 
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+});
 
 app.listen(port, () => {
     console.log('Dragon server runnig on port:', port);
